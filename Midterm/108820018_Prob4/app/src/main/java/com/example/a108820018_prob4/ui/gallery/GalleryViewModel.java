455f2452a6c817f029ec433040768decda0e0bb2
sean0505tsai <t108820018@ntut.org.tw>
2022-04-19 16:49:11 +0800

    modified Prob3

---
+++ Midterm/108820018_Prob4/app/src/main/java/com/example/a108820018_prob4/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.a108820018_prob4.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ Midterm/108820018_Prob4/app/src/main/java/com/example/a108820018_prob4/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.a108820018_prob4.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
