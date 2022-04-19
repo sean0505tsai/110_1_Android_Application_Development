55a0d62db64970db780f00974b1c3c20b004cacf
sean0505tsai <t108820018@ntut.org.tw>
2022-04-19 18:29:03 +0800

    prob1

---
+++ Midterm/108820018_Prob12/app/src/main/java/com/example/a108820018_prob1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a108820018_prob1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
